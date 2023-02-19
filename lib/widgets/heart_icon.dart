5e8eb6e70ca8f4bd14791684c4af914392c8bb7e
Yosoph Ahmed <devyosoph@gmail.com>
2023-02-19 19:33:04 +0200

    Latest Commit

--- lib/widgets/heart_icon.dart
+++ lib/widgets/heart_icon.dart
@@ -7,16 +7,67 @@ class Heart extends StatefulWidget {
   State<Heart> createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  bool isFav = false;
+
+  AnimationController? animationController;
+  Animation? colorAnimation;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      duration: const Duration(
+        milliseconds: 500,
+      ),
+      vsync: this,
+    );
+
+    colorAnimation = ColorTween(
+      begin: Colors.grey[400],
+      end: Colors.red,
+    ).animate(
+      animationController!,
+    );
+
+    animationController!.addListener(() {});
+
+    animationController!.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isFav = true;
+        });
+      }
+      if (status == AnimationStatus.dismissed) {
+        isFav = false;
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController!.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () {},
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
+    return AnimatedBuilder(
+      animation: animationController!,
+      builder: (context, _) {
+        return IconButton(
+          onPressed: () {
+            isFav
+                ? animationController!.reverse()
+                : animationController!.forward();
+          },
+          icon: Icon(
+            Icons.favorite,
+            color: colorAnimation!.value,
+            size: 30,
+          ),
+        );
+      },
     );
   }
 }
